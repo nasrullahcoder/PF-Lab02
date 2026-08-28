efd8c304df7d1c68ef129c5bc529c5d54f6bb04a
Nasrullah Coder <p263052@pwr.nu.edu.pk>
2026-08-28 21:38:37 +0500

    Create p263052_task3.c

---
+++ p263052_task3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+  printf("============== C ESCAPE SEQUENCE REFERENCE ===============\n");
+  printf("To print a double quote, write :      \\\"\n");
+  printf(" To print a literal backlash, write: \\\\\n:);
+  printf(" to inert a newline break, write: \\n\n");
+  printf(" to inert a horizontal tab stop, write: \\t\n");
+   printf(" to print a percent sign, write:  %%");
+
+return 0;
+}
+   
