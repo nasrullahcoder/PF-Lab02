7059f175e1a28d3cb5b1228cb2a44f2552c8fc0c
Nasrullah Coder <p263052@pwr.nu.edu.pk>
2026-08-28 20:25:16 +0500

    Create p2600_task2.c

---
+++ p2600_task2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main ()
+{
+int Batch year= 2026; 
+
+  printf("Batch year: %d \n", batch year);
+
+  char section ='B';
+  printf("section: %c\n", section);
+  
+Float test score: 81.2;
+printf("test score : %f\n". test score);
+
+Double semaster fee: 146000.60;  
+  printf(" double semaster: %1f\n", semaster fee);
+
+retun 0;
+}
+// Motto "Small daily steps lead to big success"
